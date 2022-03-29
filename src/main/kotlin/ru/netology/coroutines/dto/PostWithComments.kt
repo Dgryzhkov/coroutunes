6dc3df59505526c3a68e3b09711f438c65baaa1c
Dgryzhkov <dgryzhkov@gmail.com>
2022-03-29 22:55:48 +0700

    getAuthor

--- src/main/kotlin/ru/netology/coroutines/dto/PostWithComments.kt
+++ src/main/kotlin/ru/netology/coroutines/dto/PostWithComments.kt
@@ -2,5 +2,6 @@ package ru.netology.coroutines.dto
 
 data class PostWithComments(
     val post: Post,
+    val authorPost: Author,
     val comments: List<Comment>,
 )
--- src/main/kotlin/ru/netology/coroutines/main.kt
+++ src/main/kotlin/ru/netology/coroutines/main.kt
@@ -5,6 +5,7 @@ import com.google.gson.reflect.TypeToken
 import kotlinx.coroutines.*
 import okhttp3.*
 import okhttp3.logging.HttpLoggingInterceptor
+import ru.netology.coroutines.dto.Author
 import ru.netology.coroutines.dto.Comment
 import ru.netology.coroutines.dto.Post
 import ru.netology.coroutines.dto.PostWithComments
@@ -125,29 +126,31 @@ suspend fun getComments(client: OkHttpClient, id: Long): List<Comment> =
 private val gson = Gson()
 private val BASE_URL = "http://127.0.0.1:9999"
 private val client = OkHttpClient.Builder()
-    .addInterceptor(HttpLoggingInterceptor(::println).apply {
+/*    .addInterceptor(HttpLoggingInterceptor(::println).apply {
         level = HttpLoggingInterceptor.Level.BODY
-    })
+    })*/
     .connectTimeout(30, TimeUnit.SECONDS)
     .build()
 
 fun main() {
+
+
     with(CoroutineScope(EmptyCoroutineContext)) {
         launch {
             try {
-                val posts = getPosts(client)
-                    .map { post ->
-                        async {
-                            PostWithComments(post, getComments(client, post.id))
-                        }
-                    }.awaitAll()
-                println(posts)
+                val postsWithComments = getPosts(client).map { post ->
+                    async {
+                        val author = async { getAuthor(client, post.authorId) }.await()
+                        PostWithComments(post, author, getComments(client, post.id))
+                    }
+                }.awaitAll()
+                println(postsWithComments)
             } catch (e: Exception) {
                 e.printStackTrace()
             }
         }
     }
-    Thread.sleep(30_000L)
+    Thread.sleep(1_000L)
 }
 
 suspend fun OkHttpClient.apiCall(url: String): Response {
@@ -186,3 +189,6 @@ suspend fun getPosts(client: OkHttpClient): List<Post> =
 
 suspend fun getComments(client: OkHttpClient, id: Long): List<Comment> =
     makeRequest("$BASE_URL/api/slow/posts/$id/comments", client, object : TypeToken<List<Comment>>() {})
+
+suspend fun getAuthor(client: OkHttpClient, authorId: Long): Author =
+    makeRequest("$BASE_URL/api/slow/authors/$authorId", client, object : TypeToken<Author>() {})
